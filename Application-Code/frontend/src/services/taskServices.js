0e00352fc50d569f8024c66681bd165777980fdf
Jayesh Parmar <89792517+jayeshjeh@users.noreply.github.com>
2024-09-12 08:26:05 +0530

    Update taskServices.js

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
-const apiUrl = "http://k8s-threetie-mainlb-1dd958d0ec-1972088695.us-east-1.elb.amazonaws.com/"
+const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
